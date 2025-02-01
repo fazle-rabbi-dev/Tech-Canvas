fda17ef5d5b96e0fbf59cbea3ee46b6d150214cf
Fazle Rabbi <frabby983@gmail.com>
2025-02-01 15:01:59 +0600

    Fix og metadata issue for blog page

--- src/components/DynamicMetadata.jsx
+++ src/components/DynamicMetadata.jsx
@@ -10,17 +10,17 @@ function DynamicMetadata({ data }) {
   return (
     <Head>
       <title>{data?.title}</title>
-      <meta name="description" content={`${data?.desc}`} />
+      <meta name="description" content={data?.desc} />
       <meta name="keywords" content={data && keywords} />
       
       {/*Metadata for Open Graph and Twitter*/}
-      <meta property="og:title" content={`${data?.title || "Tech-Canvas - Unveiling the World of Technology, Coding, and More"}`} />
-      <meta property="og:description" content={`${data?.desc || "Explore a diverse array of insightful articles on technology, coding practices, and cutting-edge innovations at Tech-Canvas. Stay updated with the latest trends and expert insights in the tech world."}`} />
-      <meta property="og:image" content={`${data?.thumbnail || "/og.jpg"}`} />
-      <meta property="og:url" content={`https://www.tech-canvas.vercel.app/${data?.slug || ""}`} />
+      <meta property="og:title" content={data?.title} />
+      <meta property="og:description" content={data?.desc} />
+      <meta property="og:image" content={`${data?.thumbnail || "https://tech-canvas.vercel.app/og.jpg"}`} />
+      <meta property="og:url" content={`https://www.tech-canvas.vercel.app/${data?.slug}`} />
 
-      <meta property="twitter:title" content={`${data?.title || "Tech-Canvas - Unveiling the World of Technology, Coding, and More"}`} />
-      <meta property="twitter:description" content={`${data?.desc || "Explore a diverse array of insightful articles on technology, coding practices, and cutting-edge innovations at Tech-Canvas. Stay updated with the latest trends and expert insights in the tech world."}`} />
+      <meta property="twitter:title" content={data?.title} />
+      <meta property="twitter:description" content={data?.desc} />
       <meta property="twitter:image:src" content={`${data?.thumbnail || "https://tech-canvas.vercel.app/og.jpg"}`} />
     </Head>
   );
